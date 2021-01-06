ddc1af5f6cdb2e333aed44d829c41bbb3c9f7393
hossein <hosseinimf@gmail.com>
2021-01-06 23:35:30 +0100

    modified

---
+++ uppgift-1/javascript.js
@@ -0,0 +1,193 @@
+class Person {
+    constructor (firstName, lastName, email, phone, street, city, zipcode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.street = street;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+    Address() {
+        return `${this.street}, ${this.city} ${this.zipcode}`;
+    }
+    FullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+function CreatePerson() {
+    var person = new Person(
+        document.getElementById("first-name").value, 
+        document.getElementById("last-name").value, 
+        document.getElementById("email").value,
+        document.getElementById("phone").value,
+        document.getElementById("street").value,
+        document.getElementById("city").value,
+        document.getElementById("zipcode").value,
+        )
+        return person;
+}
+
+function renderLi(person) {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const lbl = document.createElement('label');
+    const btnEdit = document.createElement('button');
+    const btnTrash = document.createElement('button');
+    const divDetails = document.createElement('div');
+    divDetails.classList.add('item-details');
+    const divRow = document.createElement('div');
+    divRow.classList.add('row');
+    const divEmailPhone = document.createElement('div');
+    divEmailPhone.classList.add('email-phone', 'col-md-5');
+    const divemail = document.createElement('div');
+    const lblEmail = document.createElement('label');
+    const lblPhone = document.createElement('label');
+    const lblAddress = document.createElement('label');
+    const divPhone = document.createElement('div');
+    const divAddress = document.createElement('div');
+    divAddress.classList.add('address', 'col-md-5');
+    const divAdd = document.createElement('div');
+
+    divemail.innerText = 'Email :  ';
+    divPhone.innerText = 'Phone :  ';
+    divAdd.innerText = 'Address :  ';
+
+    lblAddress.innerText = `${person.Address()}`;
+    lblPhone.innerText = `${person.phone}`;
+    lblEmail.innerText = `${person.email}`;
+
+    divemail.append(lblEmail);
+    divPhone.append(lblPhone);
+    divAdd.append(lblAddress);
+    divEmailPhone.append(divemail, divPhone);
+    divAddress.append(divAdd);
+    divRow.append(divEmailPhone, divAddress);
+    divDetails.append(divRow);
+
+
+
+    li.classList.add('li');
+    div.classList.add('item-name');
+    btnEdit.classList.add('btn');
+    btnTrash.classList.add('btn');
+    btnEdit.innerHTML = '<i class="far fa-edit"></i>';
+    btnTrash.innerHTML = '<i class="fa fa-trash"></i>';
+    lbl.innerText = `${person.firstName} ${person.lastName}`;
+
+    btnTrash.addEventListener('click', () => { removeFunc(person) });
+    
+    div.append(lbl, btnEdit, btnTrash);
+    li.append(div, divDetails);
+
+    return li;
+}
+
+var personList = [];
+
+var submit = document.getElementById("submit");
+submit.addEventListener("click", submitFunc);
+
+function submitFunc() {
+    if (validateForm()) {
+        person1 = CreatePerson();
+        personList.push(person1);
+        const orderList = document.getElementById('list');
+        orderList.innerText = '';
+
+        for (const item of personList) {
+            orderList.append(renderLi(item));
+        }
+    } 
+}
+
+function removeFunc(person) {
+    const index = personList.indexOf(person);
+    personList.splice(index, 1);
+
+    const orderList = document.getElementById('list');
+    orderList.innerText = '';
+
+    for (const item of personList) {
+        orderList.append(renderLi(item));
+    }
+}
+
+
+const fNameInput = document.getElementById('first-name');
+const fNameErrorElement = document.getElementById('firstNameError');
+const lNameInput = document.getElementById('last-name');
+const lNameErrorElement = document.getElementById('lastNameError');
+const emailInput = document.getElementById('email');
+const emailErrorElement = document.getElementById('emailError');
+const phoneInput = document.getElementById('phone');
+const phoneErrorElement = document.getElementById('phoneError');
+const streetInput = document.getElementById('street');
+const streetErrorElement = document.getElementById('streetError');
+const cityInput = document.getElementById('city');
+const cityErrorElement = document.getElementById('cityError');
+const postalInput = document.getElementById('zipcode');
+const postalErrorElement = document.getElementById('zipcodeError');
+
+function validateInput(inputElement, errorElement, errorMessage) {
+    errorElement.innerText = '';
+    if (inputElement.value === '' || inputElement.value == null) {
+        errorElement.innerText = errorMessage;
+        return false;
+    }else
+    { return true; }
+}
+
+const validateEmail = (inputElement, errorElement, errorMessage)  => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(re.test(inputElement.value.toLowerCase())) {
+        errorElement.innerText = '';
+        return true;
+    }else {
+        errorElement.innerText = errorMessage;
+        return false;
+    }
+}
+
+const validatePhone = (phoneNumber, phoneErrorElement) => {
+    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    if (re.test(phoneNumber)){
+        phoneErrorElement.innerText = '';
+        return true;
+    }else {
+        phoneErrorElement.innerText = 'invalid phone number! Phone number must have 10-12 digits!';
+        return false;
+    }
+}
+
+const validatePostal = (inputElement, errorElement, errorMessage) => {
+    var n = inputElement.value.length;
+    errorElement.innerText = '';
+    if (n === 5){
+        return true;
+    }else {
+        errorElement.innerText = errorMessage;
+        return false;
+    }
+}
+
+const validateForm = () => {
+    var isName = validateInput(fNameInput, fNameErrorElement, 'name is required' );
+    var isFamily = validateInput(lNameInput, lNameErrorElement, 'last name is required');
+    var isEmail = validateEmail(emailInput, emailErrorElement, 'email is invalid');
+    var isPhone = validatePhone(phoneInput.value, phoneErrorElement);
+    var isStreet = validateInput(streetInput, streetErrorElement, 'street is required');
+    var isCity = validateInput(cityInput, cityErrorElement, 'city is required');
+    var isPostal = validatePostal(postalInput, postalErrorElement, 'postal code must have 5 digits');
+
+    if (isName && isFamily && isEmail && isPhone && isStreet && isCity && isPostal){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
